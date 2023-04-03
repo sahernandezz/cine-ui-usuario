cb4cf9aa977449f423306a41611a25ed1f302c4a
Sergio Alejandro Hernández Zambrano <alejo.80.123@hotmail.com>
2023-04-03 12:26:07 -0500

    init project

--- src/service/conf.ts
+++ src/service/conf.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://serversh1.ml:8029/';
-//const API = 'http://localhost:8080/';
+const API = 'http://localhost:8080/';
 
 /**
  * @description axios config
